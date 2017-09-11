d9008de1850a32eaf9247d4ac31d035b4f08d5aa
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-09-11 00:55:54 -0300

    feat: implemented not-null validator

--- lib/globals/not-null.validator.ts
+++ lib/globals/not-null.validator.ts
@@ -1,12 +1,23 @@
 import { Validator } from '../validator';
+import { ValidatorError } from '../../interfaces/validator-error';
 
+/**
+ * Checks if a value is null or undefined.
+ */
 export class NotNullValidator extends Validator {
-
   constructor() {
     super();
   }
 
-  public validate(): any {
-    throw new Error('Not implemented yet.');
+  validate(...params: any[]): Promise<ValidatorError> {
+    let response: ValidatorError;
+
+    if (!params || params.length < 1 || params[0] === null || params[0] === undefined) {
+      response = {
+        message: 'Value was null or undefined'
+      };
+    }
+
+    return Promise.resolve(response);
   }
 }
