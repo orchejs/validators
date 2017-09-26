530be7862b07eefab0d536fe285859c33eaad11b
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-09-26 17:33:46 -0300

    test: all scenarios are covered for validator-runner.

--- test/validator-runner.spec.ts
+++ test/validator-runner.spec.ts
@@ -1,23 +1,49 @@
 import { expect } from 'chai';
-import { NotNullValidator, ValidatorRunner } from '../';
+import { NotNullValidator, Validator, ValidatorError, ValidatorRunner } from '../';
+
+class ForceErrorValidator implements Validator {
+  validate(params: any): Promise<ValidatorError> {
+    throw new Error('Force Error');
+  }
+}
 
 describe('ValidatorRunner', () => {
   let validatorRunner: ValidatorRunner;
 
-  before(()=> {
+  before(() => {
     validatorRunner = new ValidatorRunner();
   });
 
   describe('#runValidations', () => {
-    it('Should resolve without errors if there are no validators', async () => {
+    it('Should execute without errors if there are no validators', async () => {
       let result = await validatorRunner.runValidations('some value', 'some field', []);
       expect(result).to.be.length(0);
     });
 
     it('Should run a simple validation that results in an error.', async () => {
-      let result = await validatorRunner.runValidations('has value', 'field1', [ { validator: NotNullValidator } ]);
+      let result = await validatorRunner.runValidations(undefined, 'field1', [
+        { validator: NotNullValidator }
+      ]);
       expect(result).to.be.length(1);
-    })
-  });
+    });
 
+    it('Should reject and interrupt the validation flow, as an exception happened', async () => {
+      let result;
+      try {
+        result = await validatorRunner.runValidations('some value', 'field1', [
+          { validator: ForceErrorValidator }
+        ]);
+      } catch (err) {
+        expect(result).to.be.undefined;
+        expect(err).to.be.not.undefined;
+      }
+    });
+
+    it('Should run without validator errors', async () => {
+      let result = await validatorRunner.runValidations('some value', 'field1', [
+        { validator: NotNullValidator }
+      ]);
+      expect(result).to.be.length(0);
+    });
+  });
 });
