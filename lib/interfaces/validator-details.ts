a6abe224265aff106f788dd7dad8c00423753751
Mauricio Gemelli Vigolo <mauriciovigolo@gmail.com>
2017-09-27 16:06:03 -0300

    docs: fixed wrong github link in documentation

--- lib/interfaces/validator-details.ts
+++ lib/interfaces/validator-details.ts
@@ -3,7 +3,7 @@
  * Copyright Mauricio Gemelli Vigolo. All Rights Reserved.
  *
  * Use of this source code is governed by a MIT-style license that can be
- * found in the LICENSE file at https://github.org/orchejs/validators/LICENSE
+ * found in the LICENSE file at https://github.com/orchejs/validators/LICENSE
  */
 
 /**
